668bbd8b319eedec546588a27cd57b10743bafd6
MattJTrueblood <MattJTrueblood@gmail.com>
2026-06-22 20:11:50 -0400

    adding versioning and config

---
+++ conf.lua
@@ -0,0 +1,8 @@
+local version = require("version")
+
+function love.conf(t)
+    t.title = "Dungeon Master v" .. version.major .. "." .. version.minor .. "." .. version.patch
+    t.version = "11.5"
+    t.window.width = 1280
+    t.window.height = 720
+end
--- main.lua
+++ main.lua
@@ -1,9 +1,12 @@
-require("lldebugger").start()
+if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
+    require("lldebugger").start()
+end
 
 local tiny = require("tiny")
+local version = require("version")
 
 local world = tiny.world()
 
 function love.draw()
-    love.graphics.print("Hello World! tiny-ecs loaded: " .. tostring(world ~= nil), 400, 300)
+    love.graphics.print("Dungeon Master v" .. version.major .. "." .. version.minor .. "." .. version.patch, 10, 10)
 end
